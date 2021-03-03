ad465ef01d7237d0a8a13400d42227735be206e1
g-abhishek <ag528927@gmail.com>
2021-03-03 13:18:16 +0530

    changes done

--- src/Components/CorporatePlan/CorporatePlanForm.js
+++ src/Components/CorporatePlan/CorporatePlanForm.js
@@ -22,7 +22,7 @@ class CorporatePlanForm extends Component {
     render() {
         return (
             <div className={style.corporatePlanDiv}>
-                <Container maxWidth="xl">
+                <Container maxWidth="xl" className={style.container}>
                     <div className={style.py3}>
                         <div className={style.card}>
                             <div className={style.cardHeader}>
@@ -78,21 +78,21 @@ function CorporateForm(props) {
                 direction="row"
             >
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Corporate Name</p>
                         <input type="text" placeholder="Corporate Name" className={style.formControl} name="corporateName" ref={register({required: true})} />
                         {errors.corporateName && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Name</p>
                         <input type="text" placeholder="Name" className={style.formControl} name="name" ref={register({required: true})} />
                         {errors.name && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Email</p>
                         <input type="email" placeholder="Email" className={style.formControl} name="email" ref={register({required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,})} />
                         {errors.email?.type === 'required' && <p className={style.textDanger}>This field is required</p>}
@@ -107,16 +107,16 @@ function CorporateForm(props) {
             >
                 
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Phone</p>
                         <input type="number" placeholder="Phone" className={style.formControl} name="phone" ref={register({required: true})} />
                         {errors.phone && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={8} md={8}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Address</p>
-                        <input type="text" placeholder="Address" className={style.formControl} name="address" ref={register({required: true})} />
+                        <textarea type="text" rows="2" placeholder="Address" className={style.textArea} name="address" ref={register({required: true})} ></textarea>
                         {errors.address && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
@@ -127,21 +127,21 @@ function CorporateForm(props) {
                 direction="row"
             >
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Department</p>
                         <input type="text" placeholder="Department" className={style.formControl} name="department" ref={register({required: true})} />
                         {errors.department && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>No. of users</p>
                         <input type="number" placeholder="No.of users" className={style.formControl} name="numberOfUsers" ref={register({required: true})} />
                         {errors.numberOfUsers && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Plan Name</p>
                         <select id="planName" className={style.formControl} name="planName" ref={register({ required: true })} style={{height:"30px"}}>
                             <option value="Plan A">Plan A</option>
@@ -158,14 +158,14 @@ function CorporateForm(props) {
                 direction="row"
             >
                 <Grid item xs={12} sm={8} md={8}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Description</p>
                         <input type="text" placeholder="Description" className={style.formControl} name="description" ref={register({required: true})} />
                         {errors.description && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Purchase Date</p>
                         <input type="datetime-local" placeholder="Purchase date and time" className={style.formControl} name="pDateTime" ref={register({required: true})} />
                         {errors.pDateTime && <p className={style.textDanger}>This field is required</p>}
@@ -178,29 +178,29 @@ function CorporateForm(props) {
                 direction="row"
             >
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Trail Start Date</p>
                         <input type="date" placeholder="Start date" className={style.formControl} name="startDate" ref={register({required: true})} />
                         {errors.startDate && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Trail End Date</p>
                         <input type="date" placeholder="End date" className={style.formControl} name="endDate" ref={register({required: true})} />
                         {errors.endDate && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={4} md={4}>
-                    <Box mx={2} mt={1}>
+                    <Box mx={2} className={style.box}>
                         <p className={style.label}>Plan Expiry Date</p>
                         <input type="date" placeholder="Expiry Date" className={style.formControl} name="expiryDate" ref={register({required: true})} />
                         {errors.expiryDate && <p className={style.textDanger}>This field is required</p>}
                     </Box>
                 </Grid>
             </Grid>
-            <Box mx={2} my={2}>
-                <p className={style.label}>List of plan Purchased</p>
+            <Box mx={2} my={1}>
+                <p className={style.tableHeader}>List of plan Purchased</p>
                 {!isMobileView ? 
                     <TableContainer>
                         <Table className={classes.table} size="small">
@@ -216,9 +216,9 @@ function CorporateForm(props) {
                                     rows.map((item) => {
                                         return (
                                             <TableRow key={item.name}>
-                                                <TableCell component="th" scope="row">{item.name}</TableCell>
-                                                <TableCell>{item.description}</TableCell>
-                                                <TableCell>{item.amount}</TableCell>
+                                                <TableCell className={style.tableItem} component="th" scope="row">{item.name}</TableCell>
+                                                <TableCell className={style.tableItem}>{item.description}</TableCell>
+                                                <TableCell className={style.tableItem}>{item.amount}</TableCell>
                                             </TableRow>
                                         )
                                         
